bb6cfa8e809642ce14899806db0b4955f1371448
Charles Melo <melocharles220@gmail.com>
2024-03-13 14:45:49 -0300

    UTILIZANDO MIDDLEWARE

--- backEnd/src/serve.js
+++ backEnd/src/serve.js
@@ -3,9 +3,15 @@ const migrationRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const express = require("express");
 const routes = require("./routes");
+const uploadConfig = require("./configs/upload");
+const cors = require("cors");
+
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
   
 app.use(routes);
 
